87e509c05b024394172676d3817ba2fb2cbc9f40
Ronan Gao <ronangao@Ronans-MacBook-Air.local>
2014-05-04 03:00:17 -0400

    fc9

--- final/data.js
+++ final/data.js
@@ -1,34 +1,21 @@
 var surgery = [
-
-  {country:"U.S.", surgery:[114062,110016,383002,8687,7140,64498,22134,4403,223066,5831,7854,40103,6426,11365,2440]},
-  {country:"Brazil", surgery:[95004,90281,213922,23361,5979,38484,11404,23311,211108,6481,28788,43809,6230,8541,9043]},
-  {country:"China", surgery:[31780,45820,77820,6780,6080,17580,7040,15260,83240,3720,10740,51680,2920,4620,4540]},
-  {country:"Japan", surgery:[28142,40996,71592,6006,5475,16094,6665,13476,72892,3296,9485,46599,2563,4083,3937]},
-  {country:"Mexico", surgery:[30208,29616,99307,4615,2474,16652,5784,2323,62010,1837,2839,13267,1898,3431,805]},
-  {country:"Italy", surgery:[20970,41790,84420,3270,2715,16095,5520,7230,54315,2400,12600,24990,3090,4320,3480]},
-  {country:"South Korea", surgery:[19800,29050,48463,4188,3700,11088,4450,9600,51200,2300,6713,31863,1800,2850,2763]},
-
+  {country:"U. S.", surgery:[20.39,25.99,10.05,10.42,3.67]},
+  {country:"Brazil", surgery:[23.32,16.46,9.97,10.50,4.84]},
+  {country:"China", surgery:[20.05,13.69,11.04,7.66,12.45]},
+  {country:"Japan", surgery:[19.55,14.01,11.00,7.55,12.50]},
+  {country:"Mexico", surgery:[20.68,24.25,9.88,10.07,4.42]},
+  {country:"Italy", surgery:[17.16,19.61,13.21,6.63,7.90]},
+  {country:"South Korea", surgery:[19.82,13.67,11.24,7.66,12.33]}
 
 ];
 
 
-var positions = 
-          [{"label":"Abdominoplasty","x":25,"y":190},
+var positions = [
+          {"label":"Abdominoplasty","x":25,"y":190},
           {"label":"Blepharoplasty","x":35,"y":40},
-          {"label":"Breast augmentation and lift","x":40,"y":130},
-          {"label":"Buttock augmentation and lift","x":55,"y":210},
-          {"label":"Chin augmentation","x":25,"y":60},
-          {"label":"Facelift","x":35,"y":50},
-          {"label":"Forehead lift","x":25,"y":20},
-          {"label":"Lip augmentation","x":25,"y":50},
+          {"label":"Breast augmentation","x":40,"y":130},
           {"label":"Lipoplasty","x":55,"y":190},
-          {"label":"Lower body lift","x":40,"y":240},
-          {"label":"Otoplasty","x":45,"y":40},
-          {"label":"Rhinoplasty","x":25,"y":40},
-          {"label":"Thigh lift","x":45,"y":260},
-          {"label":"Upper arm lift","x":65,"y":110},
-          {"label":"Vaginal rejuvenation","x":25,"y":220}
-
-          ]
+          {"label":"Rhinoplasty","x":25,"y":40}
+          ];
 
 
