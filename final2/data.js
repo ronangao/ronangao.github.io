0fabf036e65015e101a0ed7e682832353166091f
Ronan Gao <ronangao@Ronans-MacBook-Air.local>
2014-05-06 18:41:53 -0400

    fc12

---
+++ final2/data.js
@@ -0,0 +1,21 @@
+var surgery = [
+  {country:"U. S.", surgery:[20.39,25.99,10.05,10.42,3.67]},
+  {country:"Brazil", surgery:[23.32,16.46,9.97,10.50,4.84]},
+  {country:"China", surgery:[20.05,13.69,11.04,7.66,12.45]},
+  {country:"Japan", surgery:[19.55,14.01,11.00,7.55,12.50]},
+  {country:"Mexico", surgery:[20.68,24.25,9.88,10.07,4.42]},
+  {country:"Italy", surgery:[17.16,19.61,13.21,6.63,7.90]},
+  {country:"South Korea", surgery:[19.82,13.67,11.24,7.66,12.33]}
+
+];
+
+
+var positions = [
+          {"label":"Abdominoplasty","x":25,"y":190},
+          {"label":"Blepharoplasty","x":35,"y":40},
+          {"label":"Breast augmentation","x":40,"y":130},
+          {"label":"Lipoplasty","x":55,"y":190},
+          {"label":"Rhinoplasty","x":25,"y":40}
+          ];
+
+
---
+++ final5/data.js
@@ -0,0 +1,40 @@
+var surgery = [
+  
+  {country:"Brazil", sur:[211108,148962,90281,95004,43809], nonsur:[179859,79028,70487,62549,39690]},
+  {country:"China", sur:[83240,56840,45820,31780,51680], nonsur:[233500,146700,85440,36400,38960]},
+  {country:"Italy", sur:[51200,35325,29050,19800,31863], nonsur:[145688,90113,52825,23175,22225]},
+  {country:"Japan", sur:[72892,52220,40996,28142,46599], nonsur:[208313,132180,77818,33214,36437]},
+  {country:"Mexico", sur:[62010,72712,29616,30208,13267], nonsur:[204657,97774,51324,16410,26034]},
+  {country:"SouthKorea", sur:[54315,62055,41790,20970,24990], nonsur:[150285,123750,21225,16455,17025]},
+  {country:"US", sur:[223066,284351,110016,114062,40103], nonsur:[815150,404779,228361,49980,1093]}
+
+];
+
+
+var positions = [
+          {"label":"Lipoplasty","x":50,"y":240},
+          {"label":"Breast augmentation","x":30,"y":135},
+          {"label":"Blepharoplasty","x":25,"y":55},
+          {"label":"Abdominoplasty","x":18,"y":190},
+          {"label":"Rhinoplasty","x":18,"y":65}
+          ];
+
+
+var surgerynon = [
+  {country:"U. S.", nonsur:[815150,404779,228361,49980,109302]},
+  {country:"Brazil", nonsur:[179859,79028,70487,62549,39690]},
+  {country:"China", nonsur:[233500,146700,85440,36400,38960]},
+  {country:"Japan", nonsur:[208313,132180,77818,33214,36437]},
+  {country:"Mexico", nonsur:[204657,97774,51324,16410,26034]},
+  {country:"Italy", nonsur:[145688,90113,52825,23175,22225]},
+  {country:"South Korea", nonsur:[150285,123750,21225,16455,17025]}
+
+];
+
+var positionsnon = [
+          {"label":"Botox Dysport","x":18,"y":40},
+          {"label":"Hyaluronic Acid","x":80,"y":230},
+          {"label":"Laser Hair Removal","x":40,"y":330},
+          {"label":"Autologous Fat","x":30,"y":70},
+          {"label":"IPL Laser Treatment","x":40,"y":100}
+];
